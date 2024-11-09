c781fe1785d2579cb12f403a8fb875215d878220
eyabenbecher <150703203+eyabenbecher@users.noreply.github.com>
2024-11-09 21:37:38 +0100

    sound effects

--- Assets/ScriptableObjects/Inventory/scripts/Player.cs
+++ Assets/ScriptableObjects/Inventory/scripts/Player.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public InventoryObject inventory;
-    public Camera playerCamera; 
+    public Camera playerCamera;
+    public AudioClip pickupSound;        // Reference to the pickup sound clip
+
+    private AudioSource audioSource;     // Private reference to the AudioSource
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // Get the attached AudioSource
+    }
 
     void Update()
     {
         // Detect mouse click
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
             TryPickupItem();
             inventory.Save();
         }
 
-        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             inventory.Save();
@@ -35,22 +42,27 @@ public class Player : MonoBehaviour
         Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        
         if (Physics.Raycast(ray, out hit))
         {
             var item = hit.collider.GetComponent<Item>();
             if (item)
             {
-                
                 inventory.AddItem(item.item, 1);
                 Destroy(hit.collider.gameObject);
+
+                // Play the pickup sound
+                if (pickupSound) // Ensure the clip is assigned
+                {
+                    audioSource.PlayOneShot(pickupSound);
+                }
+
                 Debug.Log("Picked up " + item.item.name);
             }
         }
     }
 
-   //// private void OnApplicationQuit()
-   // {
-   //     inventory.container.Clear();
-   // }
+    //// private void OnApplicationQuit()
+    // {
+    //     inventory.container.Clear();
+    // }
 }
